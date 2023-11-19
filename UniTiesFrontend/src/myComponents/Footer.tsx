238b2a16ab14c7234ec645e399bd67d192558c41
camelCaseFollower <jacobsworkmail08@gmail.com>
2023-11-19 13:18:41 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit. On branch main Your branch is up to date with 'origin/main'. Changes to be committed:	new file:   UniTiesFrontend/src/imgs/example1.jpg	modified:   UniTiesFrontend/src/index.css	modified:   UniTiesFrontend/src/myComponents/ContactForm.tsx	modified:   UniTiesFrontend/src/myComponents/Footer.tsx	modified:   UniTiesFrontend/src/myComponents/MainBody.tsx	modified:   UniTiesFrontend/src/myComponents/evenimente/paginaEveniment.tsx	modified:   UniTiesFrontend/src/myComponents/newsletter.tsx

--- UniTiesFrontend/src/myComponents/Footer.tsx
+++ UniTiesFrontend/src/myComponents/Footer.tsx
@@ -7,22 +7,22 @@ import {Link} from "react-router-dom"
 
 export default function Footer(){
     return(
-        <div className="flex flex-wrap bg-neutral-700 w-full">
-            <div className="flex flex-wrap flex-col p-4 w-1/3 justify-center content-end">
-                <img className="h-36 w-68" src={Logo}></img>
+        <div className="flex flex-wrap bg-neutral-700 w-full justify-center">
+            <div className="flex flex-wrap flex-col p-4 w-2/3 lg:w-1/3 justify-center lg:justify-start content-center">
+                <img className="w-3/4" src={Logo}></img>
                 <div className="flex flex-wrap justify-center">
                     <img className="h-6 mr-1"src={Copyright}></img>
                     <p className="text-white">Unities CES 2023</p>
                 </div>
-            </div>
-            <div className="flex w-2/3">
-                <div className="flex flex-col w-1/3 text-xl p-8">
+            </div>{/* Left side of footer */}
+            <div className="flex w-full lg:w-2/3">
+                <div className="flex flex-col w-full lg:w-1/3 text-xl p-8">
                     <h2 className="text-white font-bold">Contact</h2>
-                    <p className="text-white">Piața Consiliul Europei 2D,<br/> 300627 Timișoara, Romania</p>
-                    <a href="" className="text-white">contact@unities.ro</a>
-                    <p className="text-white">+40 0712 277 132</p>
+                    <p className="text-white text-lg">Piața Consiliul Europei 2D,<br/> 300627 Timișoara, Romania</p>
+                    <a href="" className="text-white text-lg">contact@unities.ro</a>
+                    <p className="text-white text-lg">+40 0712 277 132</p>
                 </div>
-                <div className="flex flex-wrap w-2/3 p-8">
+                <div className="flex flex-wrap w-full lg:w-2/3 p-8">
                     <h2 className="text-xl font-bold text-white w-full  mb-1">Retele</h2>
                     <div className="flex flex-col justify-items-center items-center">
                         <Link to=""><img src={Instagram} className="w-10 hover:translate-x-2 transition-all"></img></Link>
